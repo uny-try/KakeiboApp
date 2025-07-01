76b16eada604ccd6198789984fa1745771b5435a
Tamura Yuji <uny.nippon@gmail.com>
2025-07-01 19:32:15 +0900

    Nullableプロパティを追加し、コンストラクタを削除してモデルを簡素化

--- KakeiboApp.Core/Models/Account.cs
+++ KakeiboApp.Core/Models/Account.cs
@@ -3,12 +3,7 @@ namespace KakeiboApp.Core.Models;
 public class Account
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public decimal Balance { get; set; }
 
-    public Account(string name, decimal balance)
-    {
-        Name = name;
-        Balance = balance;
-    }
 }
